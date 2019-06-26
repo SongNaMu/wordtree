1b5bbdf822c02d09370eff7ef79548fa933a160f
SongNaMu <jubal3863@naver.com>
2019-06-26 21:37:13 +0900

    make tooltip

---
+++ bg.js
@@ -0,0 +1,9 @@
+/*
+chrome.runtime.onMessage.addListener(fucntion(message){
+  if(message !== ""){
+    chrome.storage.sync.set({
+      word:message
+    });
+  }
+});
+*/
--- injectsrc/addEvent.js
+++ injectsrc/addEvent.js
@@ -3,20 +3,36 @@ var selectTxt;
 function getDragText(){
   if(document.getSelection){
     selectTxt = document.getSelection();
+    //alert("1");
   }else if(document.selection){
     selectTxt = document.selection.createRange().txt;
+    //alert("2");
   }
+  //return selectTxt;
 }
 
-var div = document.createElement('div');
-div.style.position = 'fixed';
-div.style.top = 0;
-div.style.right = 0;
-div.textContent = '';
-div.id = "txtconsole";
-document.body.appendChild(div);
+
 
 document.onmouseup = function(){
-  document.getElementById("txtconsole");
 
+  var div = document.createElement('div');
+  div.style.position = 'fixed';
+  div.style.top = 400; //window.event.clientY;
+  div.style.left = 400;//window.event.clientX;
+  div.style.backgroundColor = "black";
+  div.textContent = '';
+  div.id = "txtconsole";
+  document.body.appendChild(div);
+
+  getDragText();
+  div.innerHTML = selectTxt;
+  console.log(selectTxt);
+  alert(selectTxt);
+
+}
+document.onmousedown = function(){
+  if(document.getElementById("txtconsole")){
+    var div = document.getElementById("txtconsole");
+    document.body.removeChild(div);
+  }
 }
